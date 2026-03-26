861f3f8ae64a2fe76ffc17e7e58f47c7937f9a8a
DiogoReisDev <diogo.anatnas@gmail.com>
2026-03-26 20:46:53 -0300

    commit inicial

---
+++ bitstamp.py
@@ -0,0 +1,58 @@
+import ssl
+import json
+
+import websocket
+
+def ao_abrir(ws):
+    print("Abriu a conexão")
+
+    json_subscribe = """
+{
+    "event": "bts:subscribe",
+    "data": {
+        "channel": "live_trades_btcusd"
+    }
+}
+"""
+    ws.send(json_subscribe)
+
+
+def ao_fechar(ws):
+    print("Fechando conexão")
+
+def erro(ws, erro):
+    print("Deu erro")
+    print(erro)
+
+
+def comprar():
+    pass
+
+def vender():
+    pass
+
+def mensagem_recebida(ws, mensagem):
+    resposta_servidor = json.loads(mensagem)
+    data = resposta_servidor.get('data',{})
+
+    price = data.get('price')
+    amount = data.get('amount_str')
+
+    print(f"Alguém comprou {amount} BTC por U${price}")
+
+    if price > 9000:
+        vender()
+    elif price < 8000:
+        comprar()
+    else:
+        print("Aguarde a lógica de compra e venda")
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp("wss://ws.bitstamp.net.",
+                 on_open=ao_abrir,
+                 on_close=ao_fechar,
+                 on_message=mensagem_recebida,
+                 on_error=erro)
+
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+
